4fefe64719d843aafa52ef972a0b8c6de59a3612
vanessailieva <vanessailieva7@gmail.com>
2023-01-21 22:51:42 +0200

    initial commit

---
+++ Sum/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Sum;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Sum.Summator(new int[]{1, 2, 3}));
+    }
+}
+
---
+++ Sum/Sum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Sum
+{
+	public static class Sum
+	{
+		public static int Summator(int[] arr)
+        {
+            int sum = arr[0];
+            for (int i = 1; i < arr
+              .Length; i++)
+                sum += arr[i];
+            return sum;
+        }
+    }
+}
+
