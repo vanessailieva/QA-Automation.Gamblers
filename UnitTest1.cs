dfb76622625473d8623dde9d2098ad164ca790c6
Vanessa Ilieva <118848060+vanessailieva@users.noreply.github.com>
2023-01-22 19:25:43 +0200

    First Unit Tests

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using NUnit.Framework;
+
+namespace Summator.UnitTests
+{
+    public class SummatorTests
+    {
+        [Test]
+        public void Test_Summator_SumTwoPositiveNumber()
+        {
+            var nums = new int[] { 1, 2 };
+            var actual = Summator.Sum(nums);
+            var expected = 3;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
